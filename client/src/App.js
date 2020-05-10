8cff320e9a3ccfaf2d7f3f709970d77554789d9f
Arpan Sahoo <arpan.sahoo@ad.infosys.com>
2020-05-10 20:14:55 +0530

    Client added and have registration issue

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,19 @@ import './App.css';
 import Login from './components/auth/Login';
 import SignUp from './components/auth/SignUp'
 import { BrowserRouter as Router,Route,Switch } from 'react-router-dom'
-
-
+import { Provider } from 'react-redux'
+import store from './store'
+import Alert from './components/layout/Alert'
 const App=()=> 
    (
+     <Provider store={store}>
      <Router>
      <Fragment>
       <Navbar/>
+      >
       <Route exact path='/' component={Landing} />
       <section className="container">
+      <Alert/>
       <Switch>
           
       <Route exact path='/login' component={Login} />
@@ -26,6 +30,7 @@ const App=()=>
 
      </Fragment>
      </Router>
+     </Provider>
   );
 
 
--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React,{ Fragment,useState } from 'react'
+import {Link } from 'react-router-dom'
+
+
 
 const Login = () => {
-    return (
-        <h1>Login</h1>
-            
+
+    const [formData,setFormData] = useState({
         
+        email:'',
+        password:''
+    })
+
+    const { email,password  } = formData;
+    const onChange = e=>setFormData({
+        ...formData,
+        [e.target.name]:e.target.value
+    })
+
+    const onSubmit=e=>{
+        e.preventDefault();
+       
+            console.log(formData)
+        
+    }
+    return (
+    <Fragment>
+      <h1 className="large text-primary">Login</h1>
+      <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+      <form className="form" onSubmit={e=>onSubmit(e)}>
+        
+        <div className="form-group">
+          <input type="email" placeholder="Email Address" value={email} name="email" onChange={e=>onChange(e)} />
+         </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password" value={password}
+            minLength="6" onChange={e=>onChange(e)}
+          />
+        </div>
+        
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Do not have an account? <Link to="/signup">Register</Link>
+      </p>
+    </Fragment>
     )
 }
 
-export default Login
+export default Login;
